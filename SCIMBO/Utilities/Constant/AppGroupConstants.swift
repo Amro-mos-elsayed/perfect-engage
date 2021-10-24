f2ae5c9be3327cb84255466dd0c79d46df633291
Alabeeb <alabeeb@2p.com.sa>
2021-10-24 13:49:21 +0200

    changing group name of app

--- SCIMBO/Utilities/Constant/AppGroupConstants.swift
+++ SCIMBO/Utilities/Constant/AppGroupConstants.swift
@@ -12,5 +12,6 @@ class AppGroupConstants{
     //static let AppGroupID = "group.com.2p.Engage" // dev
     //static let AppGroupID = "group.com.2p.EngageProd" // minister
     //static let AppGroupID = "group.com.2p.EngageGitex" // gitex
-    static let AppGroupID = "group.com.2p.swcc" // SWCC
+    static let AppGroupID = "group.com.2p.swcc.engage" // SWCC
 }
+ 
